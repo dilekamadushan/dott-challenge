c8d83ba9efa6db5f9069b7fcc4f542068c8101ac
dilekamadushan <dileka.14@cse.mrt.ac.lk>
2022-02-22 23:30:23 +0530

    make the code more modularized remove loggers add unit tests

--- src/index.ts
+++ src/index.ts
@@ -1,126 +1,47 @@
 // import the required library
-// @ts-ignore
 const readline = require("readline");
-const rl = readline.createInterface({
-    // @ts-ignore
+import {calculateDistance} from "./algorithm";
+
+const readLineService = readline.createInterface({
     input: process.stdin,
-    // @ts-ignore
-    output: process.stdout,
 });
 
 let readInputLines: string[] = [];
 const answers: number[][][] = [];
-let totalTestCases: number | undefined;
+let totalTestCases: number = 0;
 let numberOfCalculations: number = 0;
-let N: number | undefined;
-let M: number | undefined;
+let N: number = 0;
+let M: number = 0;
 let countUserInput: number = 0;
-rl.on("line", (line) => {
+
+readLineService.on("line", (line: string): void => {
     readInputLines.push(line);
-    console.log("Length of readInputLines", readInputLines.length);
     countUserInput += 1;
     if (countUserInput === 1) {
         totalTestCases = parseInt(line);
         readInputLines = [];
-        console.log("Number of total lines", totalTestCases);
     } else if (readInputLines.length === 1) {
         const input: string = readInputLines[0];
         const formattedInput: number[] = input.split(" ").map((char) => parseInt(char));
         N = formattedInput[0];
         M = formattedInput[1];
-        console.log("N, M", N, M);
     } else if (readInputLines.length === N + 1) {
-        console.log("Finished taking input for this iteration", readInputLines);
         const formattedInputLines: number[][] = readInputLines
             .slice(1)
             .map((line) => line.split("").map((char) => parseInt(char)));
-        const ans = calculateDistances(N, M, formattedInputLines);
-        console.log("Answer returned", ans);
+        const ans = calculateDistance(N, M, formattedInputLines);
         answers.push(ans);
         numberOfCalculations += 1;
         readInputLines = [];
     }
-
     if (numberOfCalculations === totalTestCases) {
-        console.log("Total Test cases processed");
         printMatrix(answers);
+        process.exit(0);
     }
 });
 
-export const calculateDistances = (N: number, M: number, matrix): number[][] => {
-    console.log(matrix);
-    const rowLen = N;
-    const colLen = M;
-
-    const distances: number[][] = [];
-    const directions: number[][] = [
-        [-1, 0],
-        [1, 0],
-        [0, -1],
-        [0, 1],
-    ];
-
-    const queue = [];
-
-    // init two-dimensional array, where all ones marked with 0,
-    // and all 0's marked as Infinity;
-    // add all ones to the queue -> the main trick
-    for (let row = 0; row < rowLen; row++) {
-        distances[row] = [];
-
-        for (let col = 0; col < colLen; col++) {
-            if (matrix[row][col] === 1) {
-                queue.push([row, col]);
-                distances[row][col] = 0;
-            } else {
-                distances[row][col] = Infinity;
-            }
-        }
-    }
-
-    // now queue contains position of all ones
-    while (queue.length > 0) {
-        const node = queue.shift();
-        const row = node[0];
-        const col = node[1];
-        const value = distances[row][col];
-
-        // loop through 4 possible directions
-        for (let i = 0; i < directions.length; i++) {
-            const direction = directions[i];
-            const tempRow = row + direction[0];
-            const tempCol = col + direction[1];
-
-            // check array bounds, and also skip ones
-            if (
-                tempRow < 0 ||
-                tempRow >= rowLen ||
-                tempCol < 0 ||
-                tempCol >= colLen ||
-                matrix[tempRow][tempCol] === 1
-            )
-                continue;
-
-            // apply the minimal distance + 1
-            const isVisited = distances[tempRow][tempCol] !== Infinity;
-            distances[tempRow][tempCol] = Math.min(
-                distances[tempRow][tempCol],
-                1 + value
-            );
-
-            // add non visited positions to the queue
-            if (!isVisited) {
-                queue.push([tempRow, tempCol]);
-            }
-        }
-    }
-
-    return distances;
-};
-
-const printMatrix = (ans) => {
+const printMatrix = (ans: number[][][]): void => {
     // Printing the answer.
-    console.log("printing the answers", ans);
     for (let k = 0; k < ans.length; k++) {
         for (let i = 0; i < ans[k].length; i++) {
             let line = "";
